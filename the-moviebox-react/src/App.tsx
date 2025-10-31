226025fe02d814e004e125001b1060535e6af9a6
JoaoPedroCarvalhoDosSantos <jpcs020705@gmail.com>
2025-10-30 21:58:54 -0300

    Aula 30-10-25

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,12 +1,18 @@
-import { Header } from "./components/layout/Header"
+import { MovieContent } from "./cases/movies/components/movie-content"
 import { Footer } from "./components/layout/footer"
+import { Header } from "./components/layout/header"
+import {Navbar} from "./components/layout/navbar"
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Footer/>
+      <Header />
+      <main>
+        <Navbar />
+        <MovieContent />
+      </main>  
+      <Footer />
     </>
   )
 }
